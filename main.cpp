f8395e402013e8ed7432c628d484e582f60789c8
Jacob_Losco <jlosco2000@gmail.com>
2021-11-07 10:45:18 -0500

    fixed tie condition and removed test file

--- main.cpp
+++ main.cpp
@@ -21,28 +21,27 @@ int main() {
     } while(numRows < 4 && numCols < 4);
 
     g = new Gamespace(numRows, numCols);
-    g -> printBoard();
     while(!finished) {
-        if(numTurns == 36) {
-            player = "Tie";
-            finished = true;
-            break;
-        }
-        player = numTurns % 2 == 0 ? "Player 1" : "Player 2";
-        int chosenCol;
-        bool chosenColValid = false;
-        while(!chosenColValid) {
-            cout << player << ", choose a number 1-6 for the column you want to drop a chip into: ";
-            cin >> chosenCol;
-            if(g -> checkValidMove(chosenCol - 1)) {
-                chosenColValid = true;
-                break;
-            }
-            cout << "Invalid move... " << endl;
-        }
-        finished = g -> dropChip(numTurns % 2 == 0 ? 'r' : 'b', chosenCol - 1);
-        g -> printBoard();
-        numTurns++;
+      g -> printBoard();
+      if(numTurns == numRows * numCols) {
+          player = "Tie";
+          finished = true;
+          break;
+      }
+      player = numTurns % 2 == 0 ? "Player 1" : "Player 2";
+      int chosenCol;
+      bool chosenColValid = false;
+      while(!chosenColValid) {
+          cout << player << ", choose a number 1-6 for the column you want to drop a chip into: ";
+          cin >> chosenCol;
+          if(g -> checkValidMove(chosenCol - 1)) {
+              chosenColValid = true;
+              break;
+          }
+          cout << "Invalid move... " << endl;
+      }
+      finished = g -> dropChip(numTurns % 2 == 0 ? 'r' : 'b', chosenCol - 1);
+      numTurns++;
     }
     if(player == "Tie")
         cout << "It was a Tie!" << endl;
